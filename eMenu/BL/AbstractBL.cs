51aa00de8ed26d50d278db21a8c1a6943cb4db3c
Isuru <samarasinha.isuru@gmail.com>
2016-03-05 01:16:59 +0530

    pub to git

---
+++ eMenu/BL/AbstractBL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eMenu.DTO;
+
+namespace eMenu.BL
+{
+    public class AbstractBL <T, Idt>
+    {
+        private EntityManagerDataContext _EntityManager = new EntityManagerDataContext();
+
+        public List<T> GetAll<T>()
+        {
+            _EntityManager.DeferredLoadingEnabled = false;
+            _EntityManager.ObjectTrackingEnabled = false;
+            List<T> entities = _EntityManager.GetTable(typeof(T)) as List<T>;
+            return entities;
+        }
+
+        protected AbstractBL()
+        {
+
+        }
+
+        public bool SaveChanges()
+        {
+            try
+            {
+                _EntityManager.SubmitChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                //Logger.Error("AbstractDao", "Error In Save Changes: \n " + e.Message);
+                //Logger.Error("AbstractDao", "Error In Save Changes: \n " + e.StackTrace);
+                if (null != _EntityManager.Transaction)
+                {
+                    _EntityManager.Transaction.Rollback();
+                }
+                throw e;
+            }
+        }
+
+        protected EntityManagerDataContext ITPManager
+        {
+            get
+            {
+                if (null == _EntityManager)
+                {
+                    _EntityManager = new EntityManagerDataContext();
+                }
+                return _EntityManager;
+            }
+            set
+            {
+                _EntityManager = value;
+            }
+        } 
+    }
+}
---
+++ eMenu/Entity/SLIITCommonResource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eMenu.Entity
+{
+    public static class SLIITCommonResource
+    {
+        #region SessionVariables
+
+        public const string USER_NAME = "User_Name";
+        public const string USER_ID = "User_ID";
+        public const string USER_EMAIL = "User_Email";
+        public const string DISPLAY_NAME = "Dispaly_Name";
+        public const string DISPLAY_IMAGE = "Dispaly_Image";
+
+        #endregion
+
+
+        #region ErrorMessages
+
+        public const string ERROR_Incorrect_Credentials = "Incorrect Username or Password";
+
+
+        #endregion
+    }
+}
